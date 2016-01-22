11408da0548f4ff29b93283ebada767e4fcd5ab2
Jeff Steed <steed.jeff@gmail.com>
2016-01-21 21:14:40 -0600

    Finish log in/log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
   before_save { email.downcase! }
   validates(:name, { presence: true, length: { maximum: 50 } })
   # [\w+\-.]+@(?:[a-z\d\-]+\.)+[a-z]+ - my solution to exercise
@@ -14,4 +15,32 @@ class User < ActiveRecord::Base
 
     has_secure_password
     validates(:password, { presence: true, length: { minimum: 6 } })
+
+    # returns the hash digest of a given string
+    def User.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # returns a random token
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    # remembers a user in the database for use in persistent sessions
+    def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    # returns true if the given token matches the digest
+    def authenticated?(remember_token)
+      return false if remember_digest.nil?
+      BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    # forgets a user
+    def forget
+      update_attribute(:remember_digest, nil)
+    end
 end
